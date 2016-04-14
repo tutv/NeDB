fef7db55a210891b1f1f1fd72394d04eb88dfe5d
Tu TV <tutv95@gmail.com>
2016-04-15 00:25:53 +0700

    Updated generate posts

--- index.js
+++ index.js
@@ -184,6 +184,6 @@ app.get('/authorPosts/:id', function(req, res) {
 /**
  * Server Listen
  */
-http.listen(1234, function() {
+http.listen(2356, function() {
     console.log('listening on localhost:8001');
 });
